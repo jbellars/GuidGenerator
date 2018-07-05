79d25c89d08272df7a7aa14f334df1e17485e7fb
Justin Bellars <jbellars@gmail.com>
2018-07-05 15:29:14 -0700

    Rudimentary console app for serving up GUIDs

---
+++ GuidGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GuidGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepLooping = true;
+
+            while (keepLooping)
+            {
+                Console.WriteLine("Welcome to the GUID Generator!");
+                Console.Write("Here's a new GUID for your use: ");
+                Guid GuidToken = Guid.NewGuid();
+                Console.WriteLine(GuidToken);
+
+                Console.WriteLine("Would you like another? (Y/N)");
+                var input = Console.ReadLine();
+                if (input.ToUpper().Equals("N"))
+                    keepLooping = false;
+            }
+
+        }
+    }
+}
